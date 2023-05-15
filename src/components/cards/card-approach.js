cf99c567fe992680103c19117d75adc1b819b520
Support <support@adelphatech.com>
2023-05-16 00:18:26 +0100

    fix build v2.0

--- src/components/cards/card-approach.js
+++ src/components/cards/card-approach.js
@@ -1,12 +1,17 @@
 /** @jsx jsx */
 import { jsx, Box, Image, Heading, Text } from 'theme-ui';
 import { LearnMore } from 'components/link';
+import Svg from "react-inlinesvg"
 
 const ApproachCard = ({ data, ...props }) => {
   return (
     <Box sx={styles.feature} {...props}>
       <Box as="figure">
         <Image src={data?.icon} sx={styles.icon} alt={data?.title} />
+        <Svg
+          src={data?.icon} sx={styles.icon} title={data?.title}
+          height="auto"
+        />
       </Box>
       <Box>
         <Heading as="h4">{data?.title}</Heading>
@@ -38,7 +43,7 @@ const styles = {
       alignItems: 'center',
       minWidth: [70],
       mb: 10,
-      
+
     },
     h4: {
       fontSize: 9,
